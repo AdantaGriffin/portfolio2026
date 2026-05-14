127a0ec2339b6c0e42b4d874bf5b324eddaf2c8a
code <code@Adantas-MacBook-Air.local>
2026-05-14 15:50:48 -0400

    re imported hash-link in package.json

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,12 +1,12 @@
 import styles from './header.module.scss';
 
 import Navigation from '../navigation/navigation';
-import {HashLink} from 'react-router-hash-link';
+import {HashLink} from './react-router-hash-link';
 function Header(){
     return(
         <>
                 <header className={styles.header}>
-                    <h1><HashLink href="index">AdantaGriffin</HashLink></h1>
+                    <h1><HashLink to="index">AdantaGriffin</HashLink></h1>
                     <Navigation/>
                 </header>
         </>
