3472c57e7982077959f6156ec273b69eec08b822
Your Name <you@example.com>
2026-06-25 19:04:43 -0400

    fixed hero position

--- src/components/root/root.tsx
+++ src/components/root/root.tsx
@@ -1,12 +1,13 @@
+import styles from './root.module.scss';
 import { Outlet } from 'react-router-dom';
 import Header from '../header/header';
 
 function Root(){
     return(
-        <>
+        <section className={styles.root}>
         <Header/>
         <Outlet/>
-        </>
+        </section>
     )
 };
 
